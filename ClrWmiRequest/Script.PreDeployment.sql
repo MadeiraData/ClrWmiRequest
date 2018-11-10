19e5ebe621e1fa1dcb68b867ea438321b87d774e
Eitan Blumin <flatline972@hotmail.com>
2018-11-10 13:50:53 +0200

    Set System.Management import code as pre-deployment

---
+++ ClrWmiRequest/Script.PreDeployment.sql
@@ -0,0 +1,7 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.assemblies WHERE name = 'System.Management')
+BEGIN
+	CREATE ASSEMBLY [System.Management]
+	AUTHORIZATION [dbo]
+	FROM 'C:\windows\Microsoft.NET\Framework64\v4.0.30319\System.Management.dll'
+	WITH PERMISSION_SET = UNSAFE
+END
